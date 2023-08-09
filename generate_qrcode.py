05a90e1d0a9705f951f3b6dc3f05b2ee02946dff
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2023-08-09 15:46:22 +0530

    Create generate_qrcode.py
    add code

---
+++ generate_qrcode.py
@@ -0,0 +1,23 @@
+import qrcode
+
+# Input data for the QR code
+data = "https://www.example.com"
+
+# Generate a QR code instance
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+# Add data to the QR code
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR code instance
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image
+img.save("qrcode_example.png")
+print("QR code saved as 'qrcode_example.png'")
